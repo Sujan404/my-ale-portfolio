66e09631b441fd2f5a5ccf1e38a9f7300af44ad1
Sujan Ale <sujanale404@Sujans-MacBook-Pro.local>
2025-02-26 06:26:10 +1100

    build file

--- frontend/frontend/vite.config.ts
+++ frontend/frontend/vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-ale-portfolio/',
+  base: '/',
   plugins: [
     vue({
       template: {
