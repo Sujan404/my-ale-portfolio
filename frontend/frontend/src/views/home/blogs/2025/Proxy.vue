f59578d89926b4527f4ad1b5af198a2aebee8848
Sujan Ale <sujanale404@Sujans-MacBook-Pro.local>
2025-03-03 17:17:40 +1100

    writing about proxy server

--- frontend/frontend/src/views/home/blogs/2025/Proxy.vue
+++ frontend/frontend/src/views/home/blogs/2025/Proxy.vue
@@ -17,68 +17,60 @@
       <div class="flex justify-between">
         <div class="relative mr-8 my-5 max-md:hidden">
           <nav class="sticky top-5 bg-gray-100 rounded-xl flex flex-col p-1">
-            <div class="bg-blue-500 rounded-xl p-3">
+            <div class="bg-blue-500 rounded-xl p-3 my-1">
               <h2 class="text-lg my-3 text-white">
                 Understanding Proxy/Forward Server and Reverse Proxy Server
               </h2>
             </div>
             <a
-              class="p-3 hover:bg-gray-500 hover:rounded-xl cursor-pointer"
+              class="p-3 hover:bg-gray-500 hover:rounded-xl cursor-pointer hover:text-white"
               href="#overview"
-              :class="activeNav === 'overview' ? 'bg-gray-500 rounded-xl': 'sdf'"
+              :class="activeNav === 'overview' ? 'bg-gray-500 rounded-xl text-white' : ''"
             >
-              <span class="text-lg my-3 hover:bg-gray-500">Overview</span>
+              <span class="text-md">Overview</span>
             </a>
             <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
+              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer hover:text-white"
               href="#proxy-server"
-              :class="activeNav === 'proxy-server' ? 'bg-gray-500 rounded-xl': ''"
+              :class="activeNav === 'proxy-server' ? 'bg-gray-500 rounded-xl text-white' : ''"
             >
-              <span class="text-md my-3 hover:bg-gray-500">Forward/Proxy Server</span>
+              <span class="text-md">Forward/Proxy Server</span>
             </a>
             <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
-              href="#proxy-server-examples"
-              :class="activeNav === 'proxy-server-examples' ? 'bg-gray-500 rounded-xl': ''"
-            >
-              <span class="text-md my-3 hover:bg-gray-500">Examples of Proxy Server</span>
-            </a>
-            <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
+              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer hover:text-white"
               href="#proxy-server-features"
-              :class="activeNav === 'proxy-server-features' ? 'bg-gray-500 rounded-xl': ''"
+              :class="
+                activeNav === 'proxy-server-features' ? 'bg-gray-500 rounded-xl text-white' : ''
+              "
             >
-              <span class="text-md my-3 hover:bg-gray-500">Features and uses of Proxy Server</span>
+              <span class="text-md">Features of Proxy Server</span>
             </a>
             <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
+              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer hover:text-white"
               href="#reverse-proxy-server"
-              :class="activeNav === 'reverse-proxy-server' ? 'bg-gray-500 rounded-xl': ''"
+              :class="
+                activeNav === 'reverse-proxy-server' ? 'bg-gray-500 rounded-xl text-white' : ''
+              "
             >
-              <span class="text-md my-3 hover:bg-gray-500">Reverse Proxy Server</span>
+              <span class="text-md">Reverse Proxy Server</span>
             </a>
             <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
-              href="#reverse-proxy-server-examples"
-              :class="activeNav === 'reverse-proxy-server-examples' ? 'bg-gray-500 rounded-xl': ''"
-            >
-              <span class="text-md my-3 hover:bg-gray-500">Examples of Reverse Proxy Server</span>
-            </a>
-            <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
+              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer hover:text-white"
               href="#reverse-proxy-server-features"
-              :class="activeNav === 'reverse-proxy-server-features' ? 'bg-gray-500 rounded-xl': ''"
+              :class="
+                activeNav === 'reverse-proxy-server-features'
+                  ? 'bg-gray-500 rounded-xl text-white'
+                  : ''
+              "
             >
-              <span class="text-md my-3 hover:bg-gray-500"
-                >Features and uses of Reverse Proxy Server</span
-              >
+              <span class="text-md">Features and uses of Reverse Proxy Server</span>
             </a>
             <a
-              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer"
+              class="p-3 hover:rounded-xl hover:bg-gray-500 cursor-pointer hover:text-white"
               href="#conclusion"
-              :class="activeNav === 'conclusion' ? 'bg-gray-500 rounded-xl': ''"
+              :class="activeNav === 'conclusion' ? 'bg-gray-500 rounded-xl text-white' : ''"
             >
-              <span class="text-md my-3 hover:bg-gray-500">Conclusion</span>
+              <span class="text-md">Conclusion</span>
             </a>
           </nav>
         </div>
@@ -88,343 +80,86 @@
           </div>
           <div>
             <div id="overview">
-              <section>
-                <h1 class="text-lg my-3">Overview</h1>
+              <section class="text-justify">
+                <h1 class="text-xl my-3 font-bold">Overview</h1>
                 <h2 class="text-lg my-3">
                   These two words <strong>Proxy/Forward Proxy</strong> and
                   <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
                   heavily used in controling web traffics between users and applications servers
                   creating robust, low latency, and secured connections among users and
-                  applications.
+                  applications. Lets delve into what they are and how they are differed from each
+                  other.
                 </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
               </section>
             </div>
             <div id="proxy-server">
-              <section>
-                <h1 class="text-lg my-3">Forward/Proxy Server</h1>
+              <section class="text-justify">
+                <h1 class="text-xl my-3 font-bold">Forward/Proxy Server</h1>
                 <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
+                  Proxy server acts as a middleman between user's computer and the application
+                  server where user sends a request to browser, this request gets through the proxy
+                  server and proxy server uses its IP address to fetch the website on the behalf of
+                  user hiding user's computer IP address in the process.
                 </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
-              </section>
-            </div>
-            <div id="proxy-server-examples">
-              <section>
-                <h1 class="text-lg my-3">Examples of Proxy Server</h1>
                 <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
+                  Lets take an example, a receptionist at an office building who receives all
+                  incoming packages and then delivers them to the appropriate person inside, acting
+                  as an intermediary between the outside world and the individuals within the
+                  building; essentially, the receptionist (proxy server) manages and filters who
+                  gets what, while keeping the identities of the people inside the building (users)
+                  private from those outside.
                 </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
               </section>
             </div>
             <div id="proxy-server-features">
-              <section>
-                <h1 class="text-lg my-3">Features and uses of Proxy Server</h1>
-                <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
-                </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
+              <section class="text-justify">
+                <h1 class="text-xl my-3 font-bold">Features of Proxy Server</h1>
+                <ol class="text-lg my-3">
+                  <li>
+                    Content Filtering: Block access to specific websites or categories of content
+                    based on company policy.
+                  </li>
+                  <li>
+                    Internet Access Control: Monitor and restrict employee web browsing activity.
+                  </li>
+                  <li>Anonymity: Hide a user's IP address when accessing the internet (VPN).</li>
+                  <li>
+                    Geo-restricted Content Access: Access content from different regions by routing
+                    traffic through a proxy in that location.
+                  </li>
+                  <li>
+                    Caching Popular Content: Store frequently accessed web content locally to reduce
+                    bandwidth usage.
+                  </li>
+                </ol>
               </section>
             </div>
             <div id="reverse-proxy-server">
               <section>
-                <h1 class="text-lg my-3">Reverse Proxy Server</h1>
+                <h1 class="text-lg my-3 font-bold">Reverse Proxy Server</h1>
                 <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
+                    A reverse proxy server functions as an intermediary between users and backend servers. While a proxy server hides 
+                    a user's IP address from the internet, a reverse proxy hides the backend server's IP address from users, improving 
+                    security, load balancing, and performance.
                 </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
-              </section>
-            </div>
-            <div id="reverse-proxy-server-examples">
-              <section>
-                <h1 class="text-lg my-3">Examples of Reverse Proxy Server</h1>
-                <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
-                </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
               </section>
             </div>
             <div id="reverse-proxy-server-features">
               <section>
                 <h1 class="text-lg my-3">Features and uses of Reverse Proxy Server</h1>
                 <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
+                 
                 </h2>
-                <pre>
-                    you 
-
-
-
-
-
-
-
-
-
-
-
-
-                    need to
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    scroll
-                  </pre
-                >
               </section>
             </div>
             <div id="conclusion">
               <section>
                 <h1 class="text-lg my-3">Conclusion</h1>
                 <h2 class="text-lg my-3">
-                  These two words <strong>Proxy/Forward Proxy</strong> and
-                  <strong>Reverse Proxy</strong> are very pouplar words we hear everyday. They are
-                  heavily used in controling web traffics between users and applications servers
-                  creating robust, low latency, and secured connections among users and
-                  applications.
+                  Forward proxies act on behalf of clients in an interaction between the client and
+                  a server. Reverse proxies act on behalf of a server, in an interaction between the
+                  client and a server.
                 </h2>
-                <pre>
-                    asfasf
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    afdfds
-                </pre>
               </section>
             </div>
           </div>
@@ -439,17 +174,14 @@ import NavBar from '@/views/home/Navigation.vue'
 import Footer from '@/views/home/Footer.vue'
 import { Head } from '@unhead/vue/components'
 import { onMounted, ref, onUnmounted } from 'vue'
-import { useClipboard } from '@vueuse/core'
 import imgUrl from '@/assets/images/2025/proxy-vs-reverse-proxy.png'
 
 var activeNav = ref('')
 const sections = [
   'overview',
   'proxy-server',
-  'proxy-server-examples',
   'proxy-server-features',
   'reverse-proxy-server',
-  'reverse-proxy-server-examples',
   'reverse-proxy-server-features',
   'conclusion'
 ]
@@ -467,57 +199,10 @@ function handleNavBar() {
     var element = document.getElementById(section)
     console.log(element)
     if (element) {
-      // console.log(element.offsetHeight)
-      // console.log(element.scrollHeight)
-      console.log(element.offsetTop)
-      // console.log(window.scrollY)
       if (window.scrollY + 100 >= element.offsetTop) {
         activeNav.value = section
-        console.log('The height is more than offset height')
       }
     }
   }
-}
-// Store the code you want to copy in `source`
-const sourceOne = ref(`
-<template>
-    <div class="flex flex-col min-h-screen w-full">
-        <div class="flex flex-col flex-grow justify-center mx-auto p-4 sm:p-6 xl:p-8">
-            <h1 class="text-3xl">Oops! The page you are looking for does not exist.</h1>
-            <router-link class="text-xl" to="/">Go back to Home</router-link>
-        </div>
-    </div>
-</template>`)
-
-const sourceTwo = ref(`
-import { createRouter, createWebHistory } from 'vue-router'
-import RouteNotFound from '@/views/home/NotFound.vue'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  // Catch-all route for undefined paths
-     {
-      path: '/:pathMatch(.*)*',
-      name: 'RouteNotFound',
-      component: RouteNotFound
-    },
-  ]
-})`)
-const { text, copy, isSupported } = useClipboard()
-
-const copiedOne = ref(false)
-const copiedTwo = ref(false)
-
-const copyOne = () => {
-  copy(sourceOne.value)
-  copiedOne.value = true
-  setTimeout(() => (copiedOne.value = false), 1500) // Reset after 1.5 seconds
-}
-
-const copyTwo = () => {
-  copy(sourceTwo.value)
-  copiedTwo.value = true
-  setTimeout(() => (copiedTwo.value = false), 1500)
 }
 </script>
